5ed141caab6cfa8f2411f394ebc4297debad3a5c
Leo.Hu <lalalaleo.hu@hotmail.com>
2018-06-11 11:21:29 +0800

    doughnut容器加入

--- src/progresschart_ctrl.js
+++ src/progresschart_ctrl.js
@@ -1,4 +1,4 @@
-import {MetricsPanelCtrl} from 'app/plugins/sdk';
+import { MetricsPanelCtrl } from 'app/plugins/sdk';
 import _ from 'lodash';
 import unit from './unit';
 import kbn from 'app/core/utils/kbn';
@@ -6,73 +6,76 @@ import './css/panel.css!';
 
 export class ProgressChartCtrl extends MetricsPanelCtrl {
 
-  constructor($scope, $injector, $rootScope) {
-    super($scope, $injector);
-    this.$rootScope = $rootScope;
-    this.hiddenSeries = {};
+	constructor($scope, $injector, $rootScope) {
+		super($scope, $injector);
+		this.$rootScope = $rootScope;
+		this.hiddenSeries = {};
 
-    var panelDefaults = {
-			colorArr:['#5eb1e4','#4888e0','#2adf6e','#FFB90F','#FF4500'],
-			progressArr:[],
-			barsArr:[]
+		var panelDefaults = {
+			colorArr: ['#5eb1e4', '#4888e0', '#2adf6e', '#FFB90F', '#FF4500'],
+			progressArr: [],
+			barsArr: [],
+			doughnutsArr: [],
 		};
-		
+
 		this.dataTemp = {
-			progressArr:[],
-			barsArr:[]
+			progressArr: [],
+			barsArr: [],
+			doughnutsArr: [],
 		};
 
-    _.defaults(this.panel, panelDefaults);
+		_.defaults(this.panel, panelDefaults);
 
-    this.events.on('render', this.onRender.bind(this));
-    this.events.on('data-received', this.onDataReceived.bind(this));
-    this.events.on('data-error', this.onDataError.bind(this));
-    this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
-    this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
-  }
+		this.events.on('render', this.onRender.bind(this));
+		this.events.on('data-received', this.onDataReceived.bind(this));
+		this.events.on('data-error', this.onDataError.bind(this));
+		this.events.on('data-snapshot-load', this.onDataReceived.bind(this));
+		this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+	}
 
-  onInitEditMode() {
-    this.addEditorTab('Options', 'public/plugins/grafana-progress-panel/editor.html', 2);
-    this.unitFormats = kbn.getUnitFormats();
-  }
-  
-  setUnitFormat(subItem, obj) {	
+	onInitEditMode() {
+		this.addEditorTab('Options', 'public/plugins/grafana-progress-panel/editor.html', 2);
+		this.unitFormats = kbn.getUnitFormats();
+	}
+
+	setUnitFormat(subItem, obj) {
 		obj.format = subItem.value;
 		obj.valueShow = unit.formatValue(this.panel, obj.value, subItem.value);
-    this.render();
-  }
+		this.render();
+	}
 
-  onDataError() {
-    this.series = [];
-    this.render();
-  }
+	onDataError() {
+		this.series = [];
+		this.render();
+	}
 
-  onRender() {
-    this.data = this.parseSeries(this.series);
-  }
+	onRender() {
+		this.data = this.parseSeries(this.series);
+	}
 
-  parseSeries(series) {	
+	parseSeries(series) {
 		this.dataTemp = {
 			progressArr: unit.checkProgressArr(this.panel.progressArr, this.dataTemp.progressArr),
-			barsArr: unit.checkProgressArr(this.panel.barsArr, this.dataTemp.barsArr)
-		};	
-		if(series && series.length > 0){
+			barsArr: unit.checkProgressArr(this.panel.barsArr, this.dataTemp.barsArr),
+			doughnutsArr: unit.checkProgressArr(this.panel.doughnutsArr, this.dataTemp.doughnutsArr)
+		};
+		if (series && series.length > 0) {
 			series = unit.checkSeries(this.panel.targets, series);
 			this.dataTemp.progressArr.forEach((value, index, arr) => {
-				if(series[index] && series[index].datapoints){
+				if (series[index] && series[index].datapoints) {
 					let datapoints = series[index].datapoints;
-					if(datapoints.length > 0){
-						value.value = datapoints[datapoints.length-1][0];
+					if (datapoints.length > 0) {
+						value.value = datapoints[datapoints.length - 1][0];
 						value.valueShow = unit.formatValue(this.panel, value.value, this.panel.progressArr[index].format);
-						let perValue = datapoints[datapoints.length-1][0];
-						if(perValue>100){
+						let perValue = datapoints[datapoints.length - 1][0];
+						if (perValue > 100) {
 							perValue = 100;
 						}
-						if(perValue < 0){
+						if (perValue < 0) {
 							perValue = 0;
 						}
 						value.percent = perValue;
-					}else{
+					} else {
 						value.value = 0;
 						value.valueShow = 'N/A';
 						value.percent = 0;
@@ -81,118 +84,126 @@ export class ProgressChartCtrl extends MetricsPanelCtrl {
 			});
 
 			let total = 0,
-					proLen = this.panel.progressArr.length,
-					perTotal = 0;
-			for(var i=0; i<this.dataTemp.barsArr.length; i++){
+				proLen = this.panel.progressArr.length,
+				perTotal = 0;
+			for (var i = 0; i < this.dataTemp.barsArr.length; i++) {
 				let indTmp = proLen + i;
-				if(series[indTmp] && series[indTmp].datapoints){
-					let datapoints = series[indTmp].datapoints; 
-					if(datapoints.length > 0){
-						this.dataTemp.barsArr[i].value = datapoints[datapoints.length-1][0];
+				if (series[indTmp] && series[indTmp].datapoints) {
+					let datapoints = series[indTmp].datapoints;
+					if (datapoints.length > 0) {
+						this.dataTemp.barsArr[i].value = datapoints[datapoints.length - 1][0];
 						total = Math.round(total) + Math.round(this.dataTemp.barsArr[i].value);
 						this.dataTemp.barsArr[i].valueShow = unit.formatValue(this.panel, this.dataTemp.barsArr[i].value, this.panel.barsArr[i].format);
-					}else{
+					} else {
 						this.dataTemp.barsArr[i].value = 0;
 						this.dataTemp.barsArr[i].valueShow = 'N/A';
 					}
-				}else{
+				} else {
 					total = Math.round(total) + Math.round(this.dataTemp.barsArr[i].value);
 				}
 			}
 			this.dataTemp.barsArr.forEach((value, index, arr) => {
-				if(index == arr.length-1){
-					value.percent = 100- perTotal;
-				}else{
-					if(value.value){
-						value.percent = (value.value/total)*100;
+				if (index == arr.length - 1) {
+					value.percent = 100 - perTotal;
+				} else {
+					if (value.value) {
+						value.percent = (value.value / total) * 100;
 						value.percent = Math.floor(value.percent);
 						perTotal += value.percent;
-					}else{
+					} else {
 						value.percent = 0;
 					}
 				}
 			})
-		}else{
+			// -----------------------------------------------------------------Doughnut 数据处理-----------------------------------------------------------------
+		} else {
 			this.dataTemp.progressArr.forEach((value, index, arr) => {
 				value.value = 0;
 				value.valueShow = 'N/A';
 				value.percent = 0;
-			})	
+			})
 			this.dataTemp.barsArr.forEach((value, index, arr) => {
 				value.value = 0;
-				value.percent = index == 0? 100:0;
-				value.valueShow = 'N/A';	 
-			})	
+				value.percent = index == 0 ? 100 : 0;
+				value.valueShow = 'N/A';
+			})
+			// -----------------------------------------------------------------Doughnut 空数据处理-----------------------------------------------------------------
 		}
-    return this.dataTemp;	
-  }
-  
-  seriesHandler(seriesData) {
-		return seriesData;
-  }
+		return this.dataTemp;
+	}
 
-  onDataReceived(dataList) {
-    this.series = dataList.map(this.seriesHandler.bind(this));
-    this.data = this.parseSeries(this.series);
-    this.render(this.data);
-  }
-  
-  getProcessStyle(proObj) {
-	 	return {'width': proObj && proObj.percent? proObj.percent+"%" : 0 +'%'};
+	seriesHandler(seriesData) {
+		return seriesData;
+	}
+
+	onDataReceived(dataList) {
+		this.series = dataList.map(this.seriesHandler.bind(this));
+		this.data = this.parseSeries(this.series);
+		this.render(this.data);
+	}
+
+	getProcessStyle(proObj) {
+		return { 'width': proObj && proObj.percent ? proObj.percent + "%" : 0 + '%' };
+	}
+
+	getBarStyle(index) {
+		return { 'width': this.dataTemp.barsArr[index].percent + '%', 'background-color': this.panel.colorArr[index] };
 	}
 	
-	getBarStyle(index){
-		return {'width':this.dataTemp.barsArr[index].percent + '%','background-color': this.panel.colorArr[index]};
-	}
-  
-  addProgress() {
-	  let objTempEdit = {
-		  label:'',unit:'',type:'solid',format: 'short'
+	// -----------------------------------------------------------------Doughnut 样式获取-----------------------------------------------------------------
+
+	addProgress() {
+		let objTempEdit = {
+			label: '', unit: '', type: 'solid', format: 'short'
 		},
-		objTemp = {value:0, valueShow:'', percent:0};
+			objTemp = { value: 0, valueShow: '', percent: 0 };
 		this.panel.progressArr.push(objTempEdit);
-	  this.dataTemp.progressArr.push(objTemp);
-  }
-  
-  addBarMember() {
-	  let objTempEdit = {
-		  label:'',unit:'',format: 'short'
+		this.dataTemp.progressArr.push(objTemp);
+	}
+
+	addBarMember() {
+		let objTempEdit = {
+			label: '', unit: '', format: 'short'
 		},
-		objTemp = {value:0, valueShow:'', percent:0};
+			objTemp = { value: 0, valueShow: '', percent: 0 };
 		this.panel.barsArr.push(objTempEdit);
-	  this.dataTemp.barsArr.push(objTemp); 
-  }
-  
-  delProgress(index) {
-		this.panel.progressArr.splice(index, 1); 
-		this.dataTemp.progressArr.splice(index, 1); 
+		this.dataTemp.barsArr.push(objTemp);
+	}
+
+	// -----------------------------------------------------------------Doughnut DOM 添加-----------------------------------------------------------------
+
+	delProgress(index) {
+		this.panel.progressArr.splice(index, 1);
+		this.dataTemp.progressArr.splice(index, 1);
 		this.render();
-  }
-  
-  delBarMember(index) {
-		this.panel.barsArr.splice(index, 1); 
-		this.dataTemp.barsArr.splice(index, 1); 
-	  let total = 0;
-	  this.dataTemp.barsArr.forEach((value, index, arr) => {
-		  total = Math.round(total) + Math.round(value.value);
-	  });
-	  this.dataTemp.barsArr.forEach((value, index, arr) => {
-			if(total === 0){
-				value.percent = index==0? 100:0;
-			}else{
-				value.percent = (value.value/total)*100;
+	}
+
+	delBarMember(index) {
+		this.panel.barsArr.splice(index, 1);
+		this.dataTemp.barsArr.splice(index, 1);
+		let total = 0;
+		this.dataTemp.barsArr.forEach((value, index, arr) => {
+			total = Math.round(total) + Math.round(value.value);
+		});
+		this.dataTemp.barsArr.forEach((value, index, arr) => {
+			if (total === 0) {
+				value.percent = index == 0 ? 100 : 0;
+			} else {
+				value.percent = (value.value / total) * 100;
 			}
 		});
-		this.render();	  
-  }
-  
-  link(scope, elem) {
+		this.render();
+	}
+
+	// -----------------------------------------------------------------Doughnut DOM 删除-----------------------------------------------------------------
+	
+	link(scope, elem) {
 		this.events.on('render', () => {
 			const $panelContainer = elem.find('.panel-container');
 			const $progressPanel = elem.find('.progress-panel');
-			$progressPanel.css('height', ($panelContainer[0].offsetHeight-40)+'px');
+			$progressPanel.css('height', ($panelContainer[0].offsetHeight - 40) + 'px');
 		});
-  }
+	}
 }
 
 ProgressChartCtrl.templateUrl = 'module.html';
