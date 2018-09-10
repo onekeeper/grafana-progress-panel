cd04d5aa0ccd8d560e2822a3b6822d618232e3fc
Leo.Hu <lalalaleo.hu@hotmail.com>
2018-09-10 16:03:53 +0800

    v1.1 (#1)
    * 新增 doughnut 图形
    
    * 新增 bar2 图形

---
+++ dist/draw.js
@@ -0,0 +1,107 @@
+"use strict";
+
+System.register([], function (_export, _context) {
+    "use strict";
+
+    var defaultOpt, option, toRadian, toAngle, calLineTo, doughnut, Draw;
+    return {
+        setters: [],
+        execute: function () {
+            defaultOpt = {
+                width: 150,
+                height: 150,
+                padding: 10,
+                gutter: 0,
+                colors: ["#67C23A", //绿色 
+                "gold", "#409EFF", //蓝色 
+                "#F56C6C", //红色
+                "grey", "#E6A23C", //橙色
+                "LightPink", "black", "yellow", "white"],
+                bgColor: "#1f1d1d",
+                data: [1, 2, 3, 4]
+            };
+
+            option = function option(opt) {
+                return {
+                    dom: opt.dom,
+                    width: opt.width ? opt.width : defaultOpt.width,
+                    height: opt.height ? opt.height : defaultOpt.height,
+                    padding: defaultOpt.padding,
+                    gutter: defaultOpt.gutter,
+                    colors: defaultOpt.colors,
+                    bgColor: defaultOpt.bgColor,
+                    data: opt.data ? opt.data : defaultOpt.data
+                };
+            };
+
+            toRadian = function toRadian(angle) {
+                return angle / 180 * Math.PI;
+            };
+
+            toAngle = function toAngle(radian) {
+                return radian / Math.PI * 180;
+            };
+
+            calLineTo = function calLineTo(a, r) {
+                a = -90 + a;
+                var x = Math.cos(a * 2 * Math.PI / 360) * r;
+                var y = Math.sin(a * 2 * Math.PI / 360) * r;
+                return { x: x, y: y };
+            };
+
+            doughnut = function doughnut(opt) {
+                // console.log("draw.js/doughnut is run.");
+                // console.log("opt",opt);
+                var drawSize = opt.width <= opt.height ? opt.width : opt.height;
+                var Num = opt.data.length;
+                var cv = opt.dom;
+                var ctx = cv.getContext("2d");
+                var ctx2 = cv.getContext("2d");
+
+                // --------------------------------------//
+                var x0 = (opt.width - opt.padding * 2) / 2 + opt.padding,
+                    y0 = (opt.height - opt.padding * 2) / 2 + opt.padding,
+                    radius = (drawSize - opt.padding * 2) / 2,
+                    radiusInSide = radius * 0.75,
+                    startAngle = -90,
+                    step = 360 / Num;
+                // let colors = opt.colors.slice(0, opt.data.length);
+                var colors = opt.data;
+
+                // 绘制饼图
+                colors.forEach(function (value, index) {
+                    ctx.beginPath();
+                    ctx.fillStyle = value;
+                    ctx.moveTo(x0, y0);
+                    ctx.arc(x0, y0, radius, toRadian(startAngle), toRadian(startAngle += step));
+                    ctx.fill();
+                });
+                // 绘制分割线
+                colors.forEach(function (value, index) {
+                    var lineTo = calLineTo(step * index, radius);
+                    ctx2.beginPath();
+                    ctx2.strokeStyle = opt.bgColor;
+                    ctx2.lineWidth = radius / 20;
+                    ctx2.moveTo(x0, y0);
+                    ctx2.lineTo(x0 + lineTo.x, y0 + lineTo.y);
+                    ctx2.stroke();
+                    ctx2.closePath();
+                });
+                // 绘制内部圆
+                ctx.beginPath();
+                ctx.fillStyle = opt.bgColor;
+                ctx.moveTo(x0, y0);
+                ctx.arc(x0, y0, radiusInSide, 0, 360, false);
+                ctx.fill();
+                ctx.closePath();
+            };
+
+            _export("Draw", Draw = function Draw(opt) {
+                doughnut(option(opt));
+            });
+
+            _export("Draw", Draw);
+        }
+    };
+});
+//# sourceMappingURL=draw.js.map
--- dist/progresschart_ctrl.js
+++ dist/progresschart_ctrl.js
@@ -1,9 +1,9 @@
 'use strict';
 
-System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', './css/panel.css!'], function (_export, _context) {
+System.register(['app/plugins/sdk', './draw', 'lodash', './unit', 'app/core/utils/kbn', './css/panel.css!'], function (_export, _context) {
 	"use strict";
 
-	var MetricsPanelCtrl, _, unit, kbn, _createClass, ProgressChartCtrl;
+	var MetricsPanelCtrl, Draw, _, unit, kbn, _createClass, ProgressChartCtrl;
 
 	function _classCallCheck(instance, Constructor) {
 		if (!(instance instanceof Constructor)) {
@@ -38,6 +38,8 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 	return {
 		setters: [function (_appPluginsSdk) {
 			MetricsPanelCtrl = _appPluginsSdk.MetricsPanelCtrl;
+		}, function (_draw) {
+			Draw = _draw.Draw;
 		}, function (_lodash) {
 			_ = _lodash.default;
 		}, function (_unit) {
@@ -78,12 +80,16 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 					var panelDefaults = {
 						colorArr: ['#5eb1e4', '#4888e0', '#2adf6e', '#FFB90F', '#FF4500'],
 						progressArr: [],
-						barsArr: []
+						barsArr: [],
+						barsArr2: [],
+						doughnutsArr: []
 					};
 
 					_this.dataTemp = {
 						progressArr: [],
-						barsArr: []
+						barsArr: [],
+						barsArr2: [],
+						doughnutsArr: []
 					};
 
 					_.defaults(_this.panel, panelDefaults);
@@ -125,12 +131,18 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 					value: function parseSeries(series) {
 						var _this2 = this;
 
+						// Doughnut 测试数据
 						this.dataTemp = {
 							progressArr: unit.checkProgressArr(this.panel.progressArr, this.dataTemp.progressArr),
-							barsArr: unit.checkProgressArr(this.panel.barsArr, this.dataTemp.barsArr)
+							barsArr: unit.checkProgressArr(this.panel.barsArr, this.dataTemp.barsArr),
+							barsArr2: unit.checkProgressArr(this.panel.barsArr2, this.dataTemp.barsArr2),
+							doughnutsArr: unit.checkProgressArr(this.panel.doughnutsArr, this.dataTemp.doughnutsArr)
 						};
+						// console.log("Doughnut 获取数据 : series",series);
+						// console.log("Doughnut 获取数据 : targets",this.panel.targets);
 						if (series && series.length > 0) {
 							series = unit.checkSeries(this.panel.targets, series);
+							// -----------------------------------------------------------------Progress 数据处理-----------------------------------------------------------------
 							this.dataTemp.progressArr.forEach(function (value, index, arr) {
 								if (series[index] && series[index].datapoints) {
 									var datapoints = series[index].datapoints;
@@ -144,6 +156,16 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 										if (perValue < 0) {
 											perValue = 0;
 										}
+										// RAC视图 SQL提交 Log Buffer Hit特殊处理
+										if (series[index].target == "Log buffer space waits") {
+											if (perValue == 0) {
+												perValue = 100;
+												value.valueShow = '100%';
+											} else {
+												perValue = 90;
+												value.valueShow = '90%';
+											}
+										}
 										value.percent = perValue;
 									} else {
 										value.value = 0;
@@ -152,10 +174,10 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 									}
 								}
 							});
-
 							var total = 0,
 							    proLen = this.panel.progressArr.length,
 							    perTotal = 0;
+							// -----------------------------------------------------------------Bar 数据处理-----------------------------------------------------------------
 							for (var i = 0; i < this.dataTemp.barsArr.length; i++) {
 								var indTmp = proLen + i;
 								if (series[indTmp] && series[indTmp].datapoints) {
@@ -185,17 +207,101 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 									}
 								}
 							});
+							// -----------------------------------------------------------------Bar2 数据处理-----------------------------------------------------------------
+							var total2 = 0,
+							    perTotal2 = 0,
+							    barLen = this.panel.barsArr.length;
+							for (var i = 0; i < this.dataTemp.barsArr2.length; i++) {
+								var _indTmp = proLen + barLen + i;
+								if (series[_indTmp] && series[_indTmp].datapoints) {
+									var _datapoints = series[_indTmp].datapoints;
+									if (_datapoints.length > 0) {
+										this.dataTemp.barsArr2[i].value = _datapoints[_datapoints.length - 1][0];
+										total2 = Math.round(total2) + Math.round(this.dataTemp.barsArr2[i].value);
+										this.dataTemp.barsArr2[i].valueShow = unit.formatValue(this.panel, this.dataTemp.barsArr2[i].value, this.panel.barsArr2[i].format);
+									} else {
+										this.dataTemp.barsArr2[i].value = 0;
+										this.dataTemp.barsArr2[i].valueShow = 'N/A';
+									}
+								} else {
+									total2 = Math.round(total2) + Math.round(this.dataTemp.barsArr2[i].value);
+								}
+							}
+							this.dataTemp.barsArr2.forEach(function (value, index, arr) {
+								if (index == arr.length - 1) {
+									value.percent = 100 - perTotal2;
+								} else {
+									if (value.value) {
+										value.percent = value.value / total2 * 100;
+										value.percent = Math.floor(value.percent);
+										perTotal2 += value.percent;
+									} else {
+										value.percent = 0;
+									}
+								}
+							});
+							// -----------------------------------------------------------------Doughnut 数据处理-----------------------------------------------------------------
+							var barLen2 = this.panel.barsArr2.length;
+							if (this.dataTemp.doughnutsArr.length > 0) {
+								var dnList = this.getDoughnutList();
+								dnList = dnList.map(function (item, index) {
+									var data = ["yellow"];
+									var cursor = proLen + barLen + barLen2 + index * 2;
+									var active = series[cursor].datapoints;
+									active = active[active.length - 1][0];
+									var inactive = series[cursor + 1].datapoints;
+									inactive = inactive[inactive.length - 1][0];
+									// console.log("cursor:",cursor,"series:",series,"active",active,"inactive:",inactive);
+									for (var _i = inactive; _i > 0; _i--) {
+										data.push("#67C23A");
+									}
+									for (var _i2 = active; _i2 > 0; _i2--) {
+										data.push("red");
+									}
+									return {
+										dom: item,
+										data: data
+									};
+								});
+								this.draw(dnList);
+							}
 						} else {
+							// -----------------------------------------------------------------Progress 空数据处理-----------------------------------------------------------------
 							this.dataTemp.progressArr.forEach(function (value, index, arr) {
 								value.value = 0;
 								value.valueShow = 'N/A';
 								value.percent = 0;
 							});
+							// -----------------------------------------------------------------Bar 空数据处理-----------------------------------------------------------------
 							this.dataTemp.barsArr.forEach(function (value, index, arr) {
 								value.value = 0;
 								value.percent = index == 0 ? 100 : 0;
 								value.valueShow = 'N/A';
 							});
+							// -----------------------------------------------------------------Bar2 空数据处理-----------------------------------------------------------------
+							this.dataTemp.barsArr2.forEach(function (value, index, arr) {
+								value.value = 0;
+								value.percent = index == 0 ? 100 : 0;
+								value.valueShow = 'N/A';
+							});
+							// -----------------------------------------------------------------Doughnut 空数据处理-----------------------------------------------------------------
+							// let TestData = [[0,1], [3,2]]
+							// let dnList = this.getDoughnutList();
+							// dnList = dnList.map((item, index) => {
+							// 	let dnData = TestData[index];
+							// 	let data = ["yellow"];
+							// 	for(let i = 0;i<dnData[0];i++) {
+							// 		data.push("red");
+							// 	}
+							// 	for(let i = 0;i<dnData[1];i++) {
+							// 		data.push("#67C23A");
+							// 	}
+							// 	return {
+							// 		dom: item,
+							// 		data: data,
+							// 	}
+							// });
+							// this.draw(dnList);
 						}
 						return this.dataTemp;
 					}
@@ -222,6 +328,11 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 						return { 'width': this.dataTemp.barsArr[index].percent + '%', 'background-color': this.panel.colorArr[index] };
 					}
 				}, {
+					key: 'getBarStyle2',
+					value: function getBarStyle2(index) {
+						return { 'width': this.dataTemp.barsArr2[index].percent + '%', 'background-color': this.panel.colorArr[index] };
+					}
+				}, {
 					key: 'addProgress',
 					value: function addProgress() {
 						var objTempEdit = {
@@ -240,6 +351,31 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 						    objTemp = { value: 0, valueShow: '', percent: 0 };
 						this.panel.barsArr.push(objTempEdit);
 						this.dataTemp.barsArr.push(objTemp);
+					}
+				}, {
+					key: 'addBarMember2',
+					value: function addBarMember2() {
+						var objTempEdit = {
+							label: '', unit: '', format: 'short'
+						},
+						    objTemp = { value: 0, valueShow: '', percent: 0 };
+						this.panel.barsArr2.push(objTempEdit);
+						this.dataTemp.barsArr2.push(objTemp);
+					}
+				}, {
+					key: 'addDoughnutMember',
+					value: function addDoughnutMember() {
+						var autoID = function autoID() {
+							for (var i = 1; i > 0; i++) {
+								if (document.querySelectorAll("#doughnut_" + i).length == 0) return i;
+							}
+						};
+						var objTempEdit = {
+							id: autoID(), unit: '', label: '', format: 'short'
+						},
+						    objTemp = { value: 0, valueShow: '', percent: 0 };
+						this.panel.doughnutsArr.push(objTempEdit);
+						this.dataTemp.doughnutsArr.push(objTemp);
 					}
 				}, {
 					key: 'delProgress',
@@ -266,6 +402,79 @@ System.register(['app/plugins/sdk', 'lodash', './unit', 'app/core/utils/kbn', '.
 						});
 						this.render();
 					}
+				}, {
+					key: 'delBarMember2',
+					value: function delBarMember2(index) {
+						this.panel.barsArr2.splice(index, 1);
+						this.dataTemp.barsArr2.splice(index, 1);
+						var total = 0;
+						this.dataTemp.barsArr2.forEach(function (value, index, arr) {
+							total = Math.round(total) + Math.round(value.value);
+						});
+						this.dataTemp.barsArr2.forEach(function (value, index, arr) {
+							if (total === 0) {
+								value.percent = index == 0 ? 100 : 0;
+							} else {
+								value.percent = value.value / total * 100;
+							}
+						});
+						this.render();
+					}
+				}, {
+					key: 'delDoughnutMember',
+					value: function delDoughnutMember(index) {
+						this.panel.doughnutsArr.splice(index, 1);
+						this.dataTemp.doughnutsArr.splice(index, 1);
+						this.render();
+					}
+				}, {
+					key: 'draw',
+					value: function draw(list) {
+						// console.log("progresschart.js/draw is run.");
+						// Doughnut 测试数据
+						for (var i in list) {
+							var dom = list[i].dom;
+							dom.style.width = document.querySelectorAll("#doughnut_" + this.panel.doughnutsArr[0].id)[0].clientWidth + "px";
+							dom.style.height = '100px';
+							dom.width = window.devicePixelRatio * document.querySelectorAll("#doughnut_" + this.panel.doughnutsArr[0].id)[0].clientWidth;
+							dom.height = window.devicePixelRatio * 100;
+							Draw({
+								data: list[i].data,
+								dom: dom,
+								width: dom.width,
+								height: dom.height
+							});
+						}
+					}
+				}, {
+					key: 'getDoughnutList',
+					value: function getDoughnutList() {
+						var domList = [];
+						for (var i in this.panel.doughnutsArr) {
+							// if(document.querySelectorAll(".doughnut-contanier")[i]) {
+							// 	domList.push(document.querySelectorAll(".doughnut-contanier")[i].children[1]);
+							// }
+							var target = document.querySelectorAll("#doughnut_" + this.panel.doughnutsArr[i].id)[0];
+							// console.log("target:",target);
+							if (target) {
+								domList.push(target.children[1]);
+							}
+						}
+						return domList;
+					}
+				}, {
+					key: 'doughnutInit',
+					value: function doughnutInit(index, id, $event) {}
+					// console.log(index, id, $event);
+					// this.render();
+					// let arr = [];
+					// if(index == (len - 1)){
+					// 	arr = this.getDoughnutList();
+					// 	this.draw(arr);
+					// }
+
+					// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
 				}, {
 					key: 'link',
 					value: function link(scope, elem) {
